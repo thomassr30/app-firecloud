e1509a35be60b3cd1e4a7e0700fcc822e80cb120
Thomas Soto <thomassoto94@gmail.com>
2023-06-09 13:16:28 -0400

    change: change form and style loginScreen

--- src/pages/auth/LoginScreen.tsx
+++ src/pages/auth/LoginScreen.tsx
@@ -1,13 +1,65 @@
+import { StackScreenProps } from '@react-navigation/stack'
 import React from 'react'
-import {Platform, SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native'
+import {Platform, SafeAreaView, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native'
 
-export const LoginScreen = () => {
+interface Props extends StackScreenProps<any, any>{}
+
+export const LoginScreen = ({navigation}: Props) => {
   return (
     <SafeAreaView style={styles.AndroidSafeArea}>
-        <View>
-            <View>
-                <Text>Iniciar Sesión</Text>
+        <View style={styles.container}>
+            <View style={{alignItems: 'center'}}>
+                <Text
+                    style={styles.textTitle}
+                >Iniciar Sesión</Text>
             </View>
+        </View>
+        <View style={styles.containerForm}>
+            <TextInput 
+                placeholder='Correo'
+                placeholderTextColor={'#000'}
+                style={styles.input}
+                keyboardType='email-address'
+                autoCapitalize='none'
+                autoCorrect={false}
+            />
+
+            <TextInput 
+                placeholder='Contraseña'
+                placeholderTextColor={'#000'}
+                style={styles.input}
+                autoCapitalize='none'
+                autoCorrect={false}
+                secureTextEntry={true}
+            />
+            <View>
+                <Text
+                    style={styles.textForgotPass}
+                >
+                    ¿Olvidaste tu contraseña?
+                </Text>
+            </View>
+
+            <TouchableOpacity
+                style={styles.button}
+            >
+                <Text
+                    style={styles.titleButton}
+                >
+                    Ingresar
+                </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                style={styles.buttonRegister}
+            >
+                <Text
+                    style={styles.titleButtonRegister}
+                    onPress={() => navigation.navigate('RegisterScreen')}
+                >
+                    Crear nueva cuenta
+                </Text>
+            </TouchableOpacity>
         </View>
     </SafeAreaView>
   )
@@ -18,5 +70,61 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "white",
         paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
-      },
+    },
+    container: {
+        padding: 10 * 2,
+        marginTop: 20
+    },
+    textTitle: {
+        fontSize: 30,
+        color: '#008781',
+        fontWeight: '700',
+        marginVertical: 10 * 3
+    },
+    containerForm: {
+        marginVertical: 10 ,
+        marginHorizontal: 10 * 2
+    },
+    input: {
+        fontSize: 14,
+        padding: 10 * 2,
+        paddingLeft: 10,
+        backgroundColor: '#f1f4ff',
+        borderRadius: 10,
+        marginBottom: 10 * 2 
+    },
+    textForgotPass: {
+        fontSize: 14,
+        color: '#008781',
+        alignSelf: 'flex-end',
+        fontWeight: '700'
+    },
+    button: {
+        padding: 10 * 2,
+        backgroundColor: '#008781',
+        borderRadius: 10,
+        marginVertical: 10 * 2,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 20
+        },
+        shadowOpacity: 0.5,
+        shadowRadius: 10
+    },
+    titleButton: {
+        color: '#fff',
+        textAlign: 'center',
+        fontSize: 20
+    },
+    buttonRegister: {
+        padding: 10,
+        borderRadius: 10,
+        marginVertical: 10
+    },
+    titleButtonRegister: {
+        color: 'gray',
+        textAlign: 'center',
+        fontSize: 20
+    }
 })
